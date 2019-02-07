17d33addb4c49e1139a9052785b474322705254b
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-02-07 20:03:31 +0100

    added bcryptjs package * added User model to mongoose * added userInput * added createUser resolver * added user-event realation (user with created events)

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,11 +24,22 @@ app.use(
                 date: String!
             }
 
+            type User {
+                _id: ID!
+                email: String!
+                password: String
+            }
+
             input EventInput {
                 title: String!
                 description: String!
                 price: Float!
                 date: String!
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             type RootQuery {
@@ -35,6 +48,7 @@ app.use(
 
             type RootMutation {
                 createEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
 
             schema {
@@ -62,18 +76,50 @@ app.use(
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: '5c5c7d5aba39792e40264972'
                 });
-                return event.save().then(result => {
-                    console.log(result);
-                    return { 
-                        ...result._doc,
-                        _id: result.id
-                    };
-                }).catch(err => {
-                    console.log(err);
-                    throw err;
-                });
+                let createdEvent;
+                return event.save()
+                    .then(result => {
+                        createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                        return User.findById('5c5c7d5aba39792e40264972');
+                    })
+                    .then(user => {
+                        if (!user) {
+                            throw new Error('User doesn\'t exist!');
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then(result => {
+                        return createdEvent;
+                    })
+                    .catch(err => { throw err; });
+            },
+            createUser: args => {
+                return User.findOne({ email: args.userInput.email })
+                    .then(user => {
+                        if (user) {
+                            throw new Error('User already exists!');
+                        }
+                        return bcrypt.hash(args.userInput.password, 12)
+                    })
+                    .then(hashedPassword => {
+                        const user = new User({
+                            email: args.userInput.email,
+                            password: hashedPassword
+                        });
+                        return user.save();
+                    })
+                    .then(result => {
+                        return {
+                            ...result._doc,
+                            _id: result.id,
+                            password: null
+                        };
+                    })
+                    .catch(err => { throw err; });
             }
         },
         graphiql: true
@@ -81,7 +127,7 @@ app.use(
 );
 
 mongoose
-    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-a2ga4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-a2ga4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
     .then(() => {
         app.listen(3000);
     })
