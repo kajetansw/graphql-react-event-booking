c82b16f967609b37714ca2d7333ded628d3fb46f
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-03-05 21:47:26 +0100

    added .editorconfig * added navbar component and styled it

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
 import './App.css';
+import MainNavigation from './components/navigation/MainNavigation';
 
 class App extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Switch>
-                    <Redirect from="/" to="/auth" exact/>
-                    <Route path="/auth" component={AuthPage} />
-                    <Route path="/events" component={EventsPage} />
-                    <Route path="/bookings" component={BookingsPage} />
-                </Switch>
-            </BrowserRouter>
-        );
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
