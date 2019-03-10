c1de7484b3bc82e267440d1f63d94510c0566840
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-03-10 13:09:10 +0100

    changed eslint indentation rule to 2 spaces * added CORS handler on the backend * added and styled login/signup page * added functionality for logging and signing up on webapp

--- app.js
+++ app.js
@@ -12,20 +12,36 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+// allow CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
-    '/graphql',
-    graphqlHttp({
-        schema: graphqlSchema,
-        rootValue: graphqlResolvers,
-        graphiql: true
-    })
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true
+  })
 );
 
 mongoose
-    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-a2ga4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
-    .then(() => {
-        app.listen(3000);
-    })
-    .catch(err => console.log(err));
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-a2ga4.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch(err => console.log(err));
