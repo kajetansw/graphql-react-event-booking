4f4f10a77c43cd3ed683c9656f9d745184bac446
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-03-11 16:43:30 +0100

    added Create Event button * added modal component with backdrop linked to Create Event button * added logout button

--- webapp/src/pages/Events.jsx
+++ webapp/src/pages/Events.jsx
@@ -1,8 +1,57 @@
 import React, { Component } from 'react';
+import Modal from '../components/modal/Modal';
+import Backdrop from '../components/backdrop/Backdrop';
+import './Events.css';
 
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({
+      ...this.state,
+      creating: true
+    });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({
+      ...this.state,
+      creating: false
+    });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({
+      ...this.state,
+      creating: false
+    });
+  };
+
   render() {
-    return <h1>The Events Page</h1>;
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create event
+          </button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
