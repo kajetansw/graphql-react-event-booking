e5a80b641f003ca0e989576232dcd7a528b9c71d
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-04-22 21:01:46 +0200

    styled EventItem

--- webapp/src/components/events/event-list/event-item/EventItem.jsx
+++ webapp/src/components/events/event-list/event-item/EventItem.jsx
@@ -4,7 +4,14 @@ import './EventItem.css';
 
 const eventItem = props => (
   <li key={props.eventId} className="event__list-item">
-    {props.title}
+    <div>
+      <h1>{props.title}</h1>
+      <h2>$19.99</h2>
+    </div>
+    <div>
+      <button className="btn">View details</button>
+      <p>You're the owner of this event!</p>
+    </div>
   </li>
 );
 
